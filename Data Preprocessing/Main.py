b3d2902e64ea7c2268782fe5e8fada8de208e460
Tanuj Keshattiwar <tanujkeshattiwar@gmail.com>
2025-01-02 18:05:25 +0530

    Taken Care of all missing values in Data

--- Data Preprocessing/Main.py
+++ Data Preprocessing/Main.py
@@ -1,9 +1,22 @@
 import numpy as np
-import matplotlib as plo
+import matplotlib as plot
 import pandas as pd
 
-dataset = pd.read_csv('Data.csv');
-x = dataset.iloc[:, :-1].values
+#Importing DataSet
+dataset = pd.read_csv('Data.csv')
+
+#Dividing Dependent & Independent Features
+x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,-1].values
+
 print(x)
 print(y)
+
+#Taking Care of Missing Values
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
+imputer.fit(x[:,1:3])
+x[:,1:3] = imputer.transform(x[:,1:3])
+print("Data replaced with missing value")
+print(x)
