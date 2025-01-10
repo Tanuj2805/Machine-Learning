5bf248a85748ea32c761408949b566dbae828764
Tanuj Keshattiwar <tanujkeshattiwar@gmail.com>
2025-01-10 16:47:50 +0530

    Splitted data between train and test set

--- Simple_Linear_Regression/Simple_Linear.py
+++ Simple_Linear_Regression/Simple_Linear.py
@@ -7,3 +7,11 @@ x = dataset.iloc[:,0].values;
 y = dataset.iloc[:,1].values;
 print(x);
 print(y);
+
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=0)
+print(x_train)
+print(x_test)
+print(y_train)
+print(y_test)
